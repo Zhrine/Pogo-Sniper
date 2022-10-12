c4951fdd4c360c62ac61970727b55e855e7ca65f
Zhrine's Laptop <10787343@uvu.edu>
2022-10-12 12:49:07 -0600

    AI Started

---
+++ Pogo-Sniper/Assets/PScripts/NavAgentBehavior.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshBehavior: MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Vector3Data playerLoc;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = playerLoc.value;
+    }
+}
---
+++ Pogo-Sniper/Assets/PScripts/Vector3Data.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Vector3Data : MonoBehaviour
+{
+    public Vector3 value;
+
+    public void SetValue(Vector3 v3)
+    {
+        value = v3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
