20694dfdb5d764e2c16035d29e2f2d35ebd2ad8e
Zhrine <10787343@uvu.edu>
2022-10-26 19:42:08 -0600

    Other Scripts copied

---
+++ Pogo-Sniper/Assets/PScripts/Instancer.cs
@@ -0,0 +1,47 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu]
+public class Instancer : ScriptableObject
+{
+    public GameObject prefab;
+    private int num;
+    
+    
+    
+    public void CreateInstance()
+    {
+        Instantiate(prefab);
+    }
+
+    public void CreateInstance(Vector3Data obj)
+    {
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
+    public void CreateInstanceFromList(Vector3DataList obj)
+    {
+        foreach (var t in obj.vector3List)
+        {
+            Instantiate(prefab, t.value, Quaternion.identity);
+        }
+    }
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+    {
+        Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+        num++;
+        if (num == obj.vector3List.Count)
+        {
+            num = 0;
+        }
+    }
+    public void CreateInstanceFromListRandomly(Vector3DataList obj)
+    {
+        num = Random.Range(0, obj.vector3List.Count - 1);
+        Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+        
+    }
+    
+}
