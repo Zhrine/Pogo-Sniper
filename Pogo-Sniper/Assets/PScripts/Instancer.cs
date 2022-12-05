d48610f962ab777bb4c3c0ebcf6162733ca26bda
Zhrine's Laptop <10787343@uvu.edu>
2022-12-05 12:15:22 -0700

    Might have something

--- Pogo-Sniper/Assets/PScripts/Instancer.cs
+++ Pogo-Sniper/Assets/PScripts/Instancer.cs
@@ -1,5 +1,3 @@
-using System;
-using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
---
+++ Pogo-Sniper/Assets/PScripts/MoveUpBehavior.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using UnityEngine;
+
+public class MoveUpBehavior : MonoBehaviour
+{
+    public float seconds = 1;
+    public WaitForSeconds wfsObj;
+    public Transform playerPos;
+    private IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        yield return wfsObj;
+        transform.position = new Vector3(transform.position.x, playerPos.position.y, transform.position.z);
+    }
+
+}
